c2dd636932c427452485bfe04c0326e09affd359
Guilherme <guilherme.s.ferreira@outlook.com>
2020-03-07 00:03:12 -0300

    Update ViewController.swift
    dividir a tela sem stackView

--- TinderClone/TinderClone/ViewController.swift
+++ TinderClone/TinderClone/ViewController.swift
@@ -12,25 +12,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       
         
         let redView = UIView()
-        
-        view.addSubview(redView)
-        
         redView.backgroundColor = .red
         
-        redView.translatesAutoresizingMaskIntoConstraints = false
-//        redView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
-//        redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
-//        redView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50).isActive = true
-//        redView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
-
-        redView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        redView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
         
-        redView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        redView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        view.addSubview(redView)
+        view.addSubview(blueView)
+        
+        redView.translatesAutoresizingMaskIntoConstraints = false
+        
+        redView.topAnchor.constraint(equalTo: view.topAnchor).isActive  = true
+        redView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        redView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        redView.widthAnchor.constraint(equalToConstant: view.bounds.width / 2).isActive = true
+        
+        
+        blueView.translatesAutoresizingMaskIntoConstraints = false
+        
+        blueView.topAnchor.constraint(equalTo: view.topAnchor).isActive  = true
+        blueView.leadingAnchor.constraint(equalTo: redView.trailingAnchor).isActive = true
+        blueView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        blueView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        blueView.widthAnchor.constraint(equalToConstant: view.bounds.width / 2).isActive = true
+
         
     }
 
